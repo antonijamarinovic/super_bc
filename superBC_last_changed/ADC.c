84373f8a3a08e120ce1bb27b34a12cea4293f583
Antonija Marinović <44464694+antonijamarinovic@users.noreply.github.com>
2018-10-30 11:08:17 +0100

    Update ADC.c

--- superBC_last_changed/ADC.c
+++ superBC_last_changed/ADC.c
@@ -8,7 +8,7 @@ ADC_InitTypeDef				ADC_InitStruct;
 ADC_CommonInitTypeDef		ADC_CommonInitStruct;
 NVIC_InitTypeDef			NVIC_InitStruct;
 
-//bla
+
 		// enable the ADC interface and GPIO clock
 		RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); 			//ADC's APB interface clock
 		RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE);			// ADC2 interface clock
